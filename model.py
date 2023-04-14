a715a07aa6ad10b9d3644619987935160b0ec1cb
Yodsakit123 <123462628+Yodsakit123@users.noreply.github.com>
2023-04-14 17:28:46 +0100

    Update model.py

--- model.py
+++ model.py
@@ -12,3 +12,17 @@ class GameData:
         self.dur = 1 #player defence
         self.isUseWeapon = False #check if the weapon is use or not
         self.isUseDef = False #check if the armour is use or not
+#Class Item, contain all item data
+class Item:
+    # Code = {0:weapon, 1:equipment, 9:extra}
+    #Constructor of Item class
+    def __init__(self, name="", desc="", price=0, dur=1, atk=0, code=0,):
+        self.code = code #item code
+        self.name = name #item name
+        self.desc = desc #item description
+        self.price = price #item price
+        self.dur = dur #item defence
+        self.atk = atk #item attack damage
+        self.isUse = False #check if item is use or not
+    def __str__(self) -> str:#function to automatically return in a readable text
+        return f"{self.name} {self.desc} {self.price} {self.dur} {self.atk} {self.code} "
